1521d12ca4c2068dad1dd6fa1da238acbd0d8ac2
ulisesten <ulises.mtz.el@gmail.com>
2019-07-01 06:09:28 -0500

    Adaptacion mayor para android(modificacion del sistema entero)

--- handling/getPublicPage.js
+++
@@ -1,8 +0,0 @@
-/**ulisesten en marzo 14, 2019 */
-var util = require('../utils/loadFiles');
-
-function publicPage(req, res, csrfToken){ 
-    util.loadView('./views/publicPage.html', res, csrfToken)
-}
-
-module.exports = publicPage;
